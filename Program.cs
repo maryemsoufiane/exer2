e24cde275bcf3445d039947f276ffa440c4dc292
ELITEBOOK <ELITEBOOK@DESKTOP-7MJDDPC>
2022-10-25 11:10:52 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Prem;
+class Prem
+{
+    private string nombre;  
+
+   
+    public Prem()
+    {
+        
+    }
+
+    static void Main(string[] args)
+    {
+        Prem P = new Prem();  
+        
+    }
+}
+
+
+
+
